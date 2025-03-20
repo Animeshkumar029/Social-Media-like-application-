5d626fd556164e7474468a4c42dd84e73b40ecab
Animeshkumar029 <128279338+Animeshkumar029@users.noreply.github.com>
2025-03-20 19:14:16 +0000

    DATABASE connected

---
+++ index.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import app from "./src/app.js";
+import config from "./src/config/index.config.js";
+
+(
+    async()=>{
+        try{
+            await mongoose.connect(config.MONGO_URL);
+            console.log("✅ DB CONNECTED!");
+
+            // app.on('error',(err)=>{
+            //     console.log("Error-->",err);
+            //     throw err;
+            // });
+
+            const server=app.listen(config.PORT,()=>{
+                console.log(`listening on port ${config.PORT}`);
+            });
+
+            server.on("error",(err)=>{
+                console.log("Error-->",err);
+                process.exit(1);
+            })
+        }
+        catch(error){
+            console.log("Error-->",error);
+            throw error;
+        }
+    }
+)();
